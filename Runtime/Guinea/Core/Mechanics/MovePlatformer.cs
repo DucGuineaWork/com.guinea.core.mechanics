142dbb4cbb197ad69a493e3bb7b18cdcde5df613
Pham Duc <ducpt.guinea.work@gmail.com>
2025-01-17 14:20:21 +0700

    - Remove DOTween dependencies

--- Runtime/Guinea/Core/Mechanics/MovePlatformer.cs
+++ Runtime/Guinea/Core/Mechanics/MovePlatformer.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using DG.Tweening;
 
 
 namespace Guinea.Core.Mechanics
@@ -8,12 +7,25 @@ namespace Guinea.Core.Mechanics
     {
         [SerializeField] Vector3 m_delta;
         [SerializeField] float m_duration;
-        [SerializeField] Ease m_ease;
+        private Vector3 m_startPosition;
+        private float m_startTime;
+        private Vector3 m_delta_;
+        
 
         void Start()
         {
-            Vector3 delta = transform.TransformDirection(m_delta);
-            transform.DOMove(transform.position + delta, m_duration).SetEase(m_ease).SetLoops(-1, LoopType.Yoyo);
+            m_delta_ = transform.TransformDirection(m_delta);
+            m_startTime = Time.time;
+            m_startPosition = transform.position;
+        }
+
+        void Update()
+        {
+            float timeElapsed = (Time.time - m_startTime) % m_duration; 
+            float factor = timeElapsed / m_duration;             
+
+            Vector3 movement = Vector3.Lerp(Vector3.zero, m_delta_, Mathf.PingPong(factor, 1f));
+            transform.position = m_startPosition + movement;
         }
     }
 }
