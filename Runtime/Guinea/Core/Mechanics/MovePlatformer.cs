1e290ffc4177e47c7223dbdf65f09408b486395b
Pham Duc <ducpt.guinea.work@gmail.com>
2025-01-17 16:40:30 +0700

    * Fix moving back and forth

--- Runtime/Guinea/Core/Mechanics/MovePlatformer.cs
+++ Runtime/Guinea/Core/Mechanics/MovePlatformer.cs
@@ -8,23 +8,19 @@ namespace Guinea.Core.Mechanics
         [SerializeField] Vector3 m_delta;
         [SerializeField] float m_duration;
         private Vector3 m_startPosition;
-        private float m_startTime;
         private Vector3 m_delta_;
         
 
         void Start()
         {
             m_delta_ = transform.TransformDirection(m_delta);
-            m_startTime = Time.time;
             m_startPosition = transform.position;
         }
 
         void Update()
         {
-            float timeElapsed = (Time.time - m_startTime) % m_duration; 
-            float factor = timeElapsed / m_duration;             
-
-            Vector3 movement = Vector3.Lerp(Vector3.zero, m_delta_, Mathf.PingPong(factor, 1f));
+            float factor = Mathf.PingPong(Time.time / m_duration, 1f);          
+            Vector3 movement = Vector3.Lerp(Vector3.zero, m_delta_, factor);
             transform.position = m_startPosition + movement;
         }
     }
